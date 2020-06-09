ee67c68b9c382b99566edd316b7c69092ef76ee5
Jordan Morris <jordansnowboards@gmail.com>
2020-06-08 20:36:50 -0400

    Pulls page and returns to start page.

--- flightScraper/Program.cs
+++ flightScraper/Program.cs
@@ -2,15 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ScrapySharp.Network;
 
 namespace flightScraper
 {
     public class Program
     {
+        static ScrapingBrowser scrapingBrowser = new ScrapingBrowser();
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -22,5 +25,10 @@ namespace flightScraper
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        static HtmlNode GetHtml(string url)
+        {
+            WebPage webpage = scrapingBrowser.NavigateToPage(new Uri(url));
+            return webpage.Html;
+        }
     }
 }
--- flightScraper/Startup.cs
+++ flightScraper/Startup.cs
@@ -2,16 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ScrapySharp.Network;
 
 namespace flightScraper
 {
     public class Startup
     {
+        static ScrapingBrowser scrapingBrowser = new ScrapingBrowser();
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -32,9 +36,15 @@ namespace flightScraper
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(GetHtml("https://smithave.com/").InnerHtml);
                 });
             });
+        }
+
+        static HtmlNode GetHtml(string url)
+        {
+            WebPage webpage = scrapingBrowser.NavigateToPage(new Uri(url));
+            return webpage.Html;
         }
     }
 }
